d40f688bddecaf4a3625803a70ed5d484481b209
IDOMATH <bmtschida@stcloudstate.edu>
2020-05-04 19:51:22 -0500

    finished project

---
+++ generator/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+# Create your views here.
+def home(request):
+    return render(request, 'generator/home.html', {'password': 'hui32adsl32'})
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+def password(request):
+    the_password = ''
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    length = int(request.GET.get('length', 12))
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()_+{}:">?<'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    for x in range(length):
+        the_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': the_password})
